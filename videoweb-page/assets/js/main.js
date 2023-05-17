9188182e07557d89584f10548b298c7209fee1fb
AmilEyvazov <127213330+amileyvazov@users.noreply.github.com>
2023-05-17 22:12:25 +0200

    Update main.js

--- videoweb-page/assets/js/main.js
+++ videoweb-page/assets/js/main.js
@@ -7,10 +7,10 @@ for (let i = 0; i < res.data.length; i++) {
     row.innerHTML += `
     <div class="col-3">
         <div class="card" style="he: 18rem;">
-            <img src="${res.data[i].image}" class="card-img-top" alt="...">
+            <img src="${res.data[i].image.medium}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${res.data[i].title}</h5>
-                <p class="card-text">${res.data[i].name.length > 50 ? res.data[i].name.substring(0,50)+ " ..." : res.data[i].name}</p>
+                <p class="card-text">${res.data[i].type.length > 50 ? res.data[i].type.substring(0,50)+ " ..." : res.data[i].type}</p>
 
                 <a href="detals.html" class="btn btn-primary w-100">Detals</a>
             </div>
